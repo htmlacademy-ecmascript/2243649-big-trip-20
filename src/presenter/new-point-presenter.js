331871233792f553aff2888ed40ce86f6fe491dd
Виктория <mihailova-vik@mail.ru>
2023-06-21 20:24:46 +0300

    Homework 13."Включит" создание и удаление на сервере

--- src/presenter/new-point-presenter.js
+++ src/presenter/new-point-presenter.js
@@ -51,7 +51,7 @@ export default class NewPointPresenter {
     this.#handleDataChange(
       UserAction.ADD_POINT,
       UpdateType.MINOR,
-      {id: nanoid(), ...point},
+      point,
     );
     this.destroy();
   };
